32c08989ef47e0ea5e373845718f21f500205975
Carlos Castillo <niquepa@gmail.com>
2017-10-06 17:46:46 -0400

    working with git

--- Gemfile
+++ Gemfile
@@ -2,5 +2,10 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
+gem 'slack-ruby-bot'
+gem 'celluloid-io'
+gem 'giphy'
+gem 'octokit', '~> 4.0'
+
 # Specify your gem's dependencies in dot_slack_bot.gemspec
 gemspec
--- lib/dot_slack_bot.rb
+++ lib/dot_slack_bot.rb
@@ -1,5 +1,17 @@
 require "dot_slack_bot/version"
 
-module DotSlackBot
-  # Your code goes here...
+module DotSlackBot < SlackRubyBot::Bot
+
+
+  command 'ping' do |client, data, match|
+    client.say(text: 'pong', channel: data.channel)
+  end
+
+  # class PongBot < SlackRubyBot::Bot
+  #   command 'ping' do |client, data, match|
+  #     client.say(text: 'pong', channel: data.channel)
+  #   end
+  # end
+
+  # PongBot.run
 end
